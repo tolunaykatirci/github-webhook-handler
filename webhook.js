857548e1cf646eedbecbc8f13102c032d0e93b4b
tolunaykatirci <tolunaykatirci@gmail.com>
2021-06-15 23:33:51 +0300

    add body-parser, crypto

--- webhook.js
+++ webhook.js
@@ -1,19 +1,42 @@
-var express = require('express')
+const fs = require('fs');
+const express = require('express')
+const crypto = require('crypto')
 
-function checkAuth(req) {
-    return true
+const mySecretRaw = fs.readFileSync('mysecret.json');
+const mySecret = JSON.parse(mySecretRaw);
+const sigHeaderName = 'X-Hub-Signature-256'
+const sigHashAlg = 'sha256'
+
+function verifyPostData(req, res, next) {
+    if (!req.rawBody) {
+        return next('Request body empty')
+      }
+    
+      const sig = Buffer.from(req.get(sigHeaderName) || '', 'utf8')
+      const hmac = crypto.createHmac(sigHashAlg, mySecret)
+      const digest = Buffer.from(sigHashAlg + '=' + hmac.update(req.rawBody).digest('hex'), 'utf8')
+      if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
+        return next(`Request body digest (${digest}) did not match ${sigHeaderName} (${sig})`)
+      }
+    
+      return next()
 }
 
 var app = express()
- 
+app.use(express.json({
+    verify: (req, res, buf, encoding) => {
+        if (buf && buf.length) {
+          req.rawBody = buf.toString(encoding || 'utf8');
+        }
+      }
+}))
+
 app.get('/', function (req, res) {
    res.send('Hello, stranger!')
 })
 
-app.post('/', function (req, res) {
-    console.log(JSON.stringify(req))
-    res.send('OK!')
+app.post('/', verifyPostData, function (req, res) {
+    res.status(200).send('Request body was signed')
 })
 
-
-var server = app.listen(8080)
+app.listen(8080)
